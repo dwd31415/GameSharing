35a06d467f859df358b4b2fb46dd9beeddf4f194
Adrian Dawid <adriandwd@gmail.com>
2015-04-15 18:59:36 -0700

    * Updated copyright annotations

--- Objective-c++/GameCenterHelper.mm
+++ Objective-c++/GameCenterHelper.mm
@@ -1,7 +1,7 @@
 /*
 GameCenterHelper.mm
 
-Copyright 2014 Adrian Dawid
+Copyright 2015 Adrian Dawid
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
